1de48527ef1d0be60919a7ef369111f2630f02ce
saurkmez <77157480+saurkmez@users.noreply.github.com>
2021-06-10 14:41:41 +0300

    photo

--- src/components/Resume1/Photo.js
+++ src/components/Resume1/Photo.js
@@ -2,24 +2,36 @@ import React, { useState } from "react";
 
 function Photo() {
   const [photo, setPhoto] = useState("Fotoğraf Ekle");
+
   const photoHandler = (e) => {
-    const selected = e.target.files[0];
-    const ALLOWED_TYPES = ["image/png", "image/jpeg", "image/jpg"];
+    const selected = e.target.files[0]; //İlk dosyayı alır
+    const ALLOWED_TYPES = ["image/png", "image/jpeg", "image/jpg"]; // izin verilen dosya tipleri
     if (selected && ALLOWED_TYPES.includes(selected.type)) {
-      let reader = new FileReader();
+      // includes, array içerisindeki istedi seçtiğimiz dosyanın tipini bulur.
+      //selected.type dizininde selected yüklediğimiz dosya,type ise dosya tipi. include ise array içerisindeki dosya tiplerine bakıyor.
+      let reader = new FileReader(); //FileReader API çalıştırıyoruz.Dosyaları asenkron okumamızı sağlar
+      reader.readAsDataURL(selected); // Datayı URL olarak okuma metodu.
       reader.onloadend = () => {
+        // onloadend, okumayı bitirdiğimiz dosyayı sayfaya yüklemeyi çalıştıran fonksiyon
         setPhoto(reader.result);
       };
-      reader.readAsDataURL(selected);
     } else {
       console.log("yanlış");
     }
   };
-
+  const removeHandler = () => {
+    setPhoto("");
+  };
   return (
     <div className="photocontainer">
-      <div className="photo" style={{ background: `url("${photo}")` }}></div>
+      <div
+        className="photo"
+        style={{
+          background: `url("${photo}") no-repeat center/cover`,
+        }}
+      ></div>
       <input type="file" name="Add Photo" id="" onChange={photoHandler} />
+      <button onClick={removeHandler}>Remove Photo</button>
     </div>
   );
 }
